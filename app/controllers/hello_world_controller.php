76ee983643012f2cfb68fb51379236758bf04221
Juha-Pekka Moilanen <juhapekm@dhcp-asv-114.helsinki.fi>
2016-01-29 11:27:08 +0200

    Viikko1 täydennykset ja viikko 2 80%

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,25 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  //orig View::make('home.html');
+      echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      //orig echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+    public static function item_list(){
+      View::make('suunnitelmat/item_list.html');
+    }
+    public static function item_show(){
+      View::make('suunnitelmat/item_show.html');
+    }
+    public static function item_edit(){
+      View::make('suunnitelmat/item_edit.html');
+    }
+    public static function login(){
+      View::make('suunnitelmat/login.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,19 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
+
+  $routes->get('/items', function() {
+    HelloWorldController::item_list();
+  });
+
+  $routes->get('/items/1', function() {
+    HelloWorldController::item_show();
+  });
+
+  $routes->get('/items/1/edit', function() {
+    HelloWorldController::item_edit();
+  });
+
